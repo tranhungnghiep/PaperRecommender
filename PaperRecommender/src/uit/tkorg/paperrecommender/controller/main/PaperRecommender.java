fe1a3989407bd500cd494bdcb7d67779a79d2834
tranhungnghiep <tranhungnghiep@gmail.com>
2014-03-12 12:31:59 +0000

    Initially import project to Google code repository.

---
+++ PaperRecommender/src/uit/tkorg/paperrecommender/controller/main/PaperRecommender.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.tkorg.paperrecommender.controller.main;
+
+/**
+ *
+ * @author THNghiep
+ * Main entry class.
+ */
+public class PaperRecommender {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
