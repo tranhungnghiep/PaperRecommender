8e8b6985df984add5d080d03a2252c8dd82fdc4d
tranhungnghiep <tranhungnghiep@gmail.com>
2014-03-18 09:03:53 +0000

    Repair error.

---
+++ PaperRecommender/src/uit/tkorg/paperrecommender/controller/central/PaperRecommender.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.tkorg.paperrecommender.controller.central;
+
+import uit.tkorg.paperrecommender.gui.PaperRecommendation;
+
+/**
+ *
+ * @author THNghiep
+ * Main entry class.
+ */
+public class PaperRecommender {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PaperRecommendation paper_recommend=new PaperRecommendation();
+        paper_recommend.show();
+    }
+}
