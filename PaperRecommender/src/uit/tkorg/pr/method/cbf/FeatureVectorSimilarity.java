08c4f15782cc013f55678a287d4f03a8d2fa7651
tin.huynh.uit <tin.huynh.uit@gmail.com>
2014-06-05 05:14:55 +0000

    Parallel computing similarity of authors and papers for CB Recommendation Method

--- PaperRecommender/src/uit/tkorg/pr/method/cbf/FeatureVectorSimilarity.java
+++ PaperRecommender/src/uit/tkorg/pr/method/cbf/FeatureVectorSimilarity.java
@@ -5,79 +5,106 @@
 package uit.tkorg.pr.method.cbf;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import uit.tkorg.pr.model.Author;
 import uit.tkorg.pr.model.Paper;
 import uit.tkorg.utility.general.HashMapUtility;
 import uit.tkorg.utility.general.WeightingUtility;
 
 /**
- * This class handles logic for recommending papers to each author.
- * Data: list of authors used as a universal recommendation list.
- * Method: 
- * - generateRecommendationForAllAuthors: 
- * + input: list of authors, list of papers.
- * + output: list of authors with recommendation list included, also includes all data of the input list of authors.
- * This output could be used as universal recommendation list, upto the input list of authors.
+ * This class handles logic for recommending papers to each author. Data: list
+ * of authors used as a universal recommendation list. Method: -
+ * generateRecommendationForAllAuthors: + input: list of authors, list of
+ * papers. + output: list of authors with recommendation list included, also
+ * includes all data of the input list of authors. This output could be used as
+ * universal recommendation list, upto the input list of authors.
+ *
  * @author THNghiep
  */
 public class FeatureVectorSimilarity {
-    
+
     // Prevent instantiation.
     private FeatureVectorSimilarity() {
     }
 
     /**
      * This method runs content based recommendation business for all authors.
-     * 
+     *
      * @param authors: all authors in the test set.
      * @param papers: all papers.
      * @param similarityScheme 0: cosine
      * @param n: top n item to recommend.
-     * 
-     * - For each author:
-     * + Compute similarity with all papers.
-     * + Sort list of papers, based on similarity.
-     * + Take top n papers with highest similarity for the recommendation list.
-     * + Save recommendation list into current author.
+     *
+     * - For each author: + Compute similarity with all papers. + Sort list of
+     * papers, based on similarity. + Take top n papers with highest similarity
+     * for the recommendation list. + Save recommendation list into current
+     * author.
      */
-    public static void generateRecommendationForAllAuthors(HashMap<String, Author> authors, HashMap<String, Paper> papers, 
-            int similarityScheme, int n) throws Exception {
+    public static void generateRecommendationForAllAuthors(final HashMap<String, Author> authors, final HashMap<String, Paper> papers,
+            final int similarityScheme, final int n) throws Exception {
+        Runtime runtime = Runtime.getRuntime();
+        int numOfProcessors = runtime.availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(numOfProcessors - 1);
+
+        System.out.println("NUM OF AUTHOR:" + authors.size());
+        int count = 0;
         for (String authorId : authors.keySet()) {
-            authors.get(authorId).setRecommendationList(generateRecommdation(authors.get(authorId), papers, similarityScheme, n));
+            System.out.println((new Date(System.currentTimeMillis()).toString()) + "...PROCESSING:" + count++);
+            final Author authorObj = authors.get(authorId);
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        generateRecommendation(authorObj, papers, similarityScheme, n);
+                    } catch (Exception ex) {
+                        Logger.getLogger(FeatureVectorSimilarity.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
+        }
+
+        executor.shutdown();
+        while (!executor.isTerminated()) {
         }
     }
 
     /**
      * This method build list of top n papers to recommend to an author.
-     * 
+     *
      * @param author: current author
      * @param papers: hashmap of all papers to recommend.
      * @param similarityScheme 0: cosine
      * @param n: top n item to recommend.
-     * 
+     *
      * @return recommendationPapers
      */
-    private static List<String> generateRecommdation(Author author, HashMap<String, Paper> papers, int similarityScheme, int n) throws Exception {
+    private static void generateRecommendation(Author author, HashMap<String, Paper> papers, int similarityScheme, int n)
+            throws Exception {
+
         List<String> recommendedPapers = new ArrayList<>();
-        HashMap<String, Double> paperSimilarity = new HashMap();
-        
+        HashMap<String, Double> paperSimilarityHM = new HashMap(); // <IDPaper, SimValue>
+
         // Compute similarities between current author and all papers.
         if (similarityScheme == 0) {
             for (String key : papers.keySet()) {
                 Double similarity = new Double(WeightingUtility.computeCosine(author.getFeatureVector(), papers.get(key).getFeatureVector()));
-                paperSimilarity.put(key, similarity);
+                paperSimilarityHM.put(key, similarity);
             }
         }
-        
+
         // Sort papers descending based on similarity.
-        LinkedHashMap<String, Double> sortedPaperSimilarity = HashMapUtility.getSortedMapDescending(paperSimilarity);
-        
+        LinkedHashMap<String, Double> sortedPaperSimilarityHM = HashMapUtility.getSortedMapDescending(paperSimilarityHM);
+
         // Take top n papers.
         int counter = 0;
-        for (String paperId : sortedPaperSimilarity.keySet()) {
+        for (String paperId : sortedPaperSimilarityHM.keySet()) {
             recommendedPapers.add(paperId);
             counter++;
             if (counter >= n) {
@@ -85,6 +112,6 @@ public class FeatureVectorSimilarity {
             }
         }
         
-        return recommendedPapers;
+        author.setRecommendationList(recommendedPapers);
     }
 }
